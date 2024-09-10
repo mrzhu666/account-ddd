9aa12558b6028032cc45aa58cf62337c11f7a26e
mrzhu666 <1052712811@qq.com>
2024-09-10 16:38:28 +0800

    docs: comment knife4j

--- account-adapter/src/main/java/org/mrzhuyk/practice/controller/UserController.java
+++ account-adapter/src/main/java/org/mrzhuyk/practice/controller/UserController.java
@@ -26,27 +26,28 @@ public class UserController {
     
     @Operation(summary = "查询用户")
     @Parameters({
-        @Parameter(name="userId",description = "用户id",required = true,in = ParameterIn.QUERY)
+        @Parameter(name = "userId", description = "用户id", required = true, in = ParameterIn.QUERY)
     })
     @GetMapping("/query")
     public Response<UserInfoVO> query(Long userId) {
         return Response.success(userInfoByIdQueryExe.execute(userId));
     }
     
+    
     @PostMapping("/register")
+    @Operation(summary = "用户注册", description = "用户注册成功返回用户id")
     public Response<Long> register(@RequestBody UserRegisterCmd userRegisterCmd) {
-        
         return Response.success(userRegisterCmdExe.execute(userRegisterCmd));
     }
     
     @Operation(summary = "测试接口")
     @Parameters({
-        @Parameter(name="testId",description = "测试id",required = true,in = ParameterIn.QUERY),
-        @Parameter(name="testName",description = "测试名",required = true,in=ParameterIn.QUERY)
+        @Parameter(name = "testId", description = "测试id", required = true, in = ParameterIn.QUERY),
+        @Parameter(name = "testName", description = "测试名", required = true, in = ParameterIn.QUERY)
     })
     @GetMapping("/hello")
     public Response<String> test(String testId, String testName) {
-        return Response.success(testId+" "+testName);
+        return Response.success(testId + " " + testName);
     }
-
+    
 }
--- account-common/account-adapter-starter/src/main/java/org/mrzhuyk/practice/config/Response.java
+++ account-common/account-adapter-starter/src/main/java/org/mrzhuyk/practice/config/Response.java
@@ -1,17 +1,27 @@
 package org.mrzhuyk.practice.config;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.Data;
 
 /**
  * 返回数据，响应封装
  * @param <T> 响应数据，异常时为空
  */
+
 @Data
 public class Response<T> {
+    
+    @Schema(description = "业务响应码")
     private int code;
+    
+    @Schema(description = "业务响应信息")
     private String message;
+    
+    @Schema(description = "业务响应数据")
     private T data;
+    
     //业务执行成功
     public static <T> Response<T> success(T data){
         Response<T> response=new Response<>();
