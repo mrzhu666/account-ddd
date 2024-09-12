2d8e53fa23c4282a3745f589e209587ba8f27baf
mrzhu666 <1052712811@qq.com>
2024-09-12 17:23:13 +0800

    refactor: UserEncryptPassword modify encryption

--- account-domain/src/main/java/org/mrzhuyk/practice/domain/user/model/UserEncryptPassword.java
+++ account-domain/src/main/java/org/mrzhuyk/practice/domain/user/model/UserEncryptPassword.java
@@ -2,13 +2,14 @@ package org.mrzhuyk.practice.domain.user.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.DigestUtils;
 
 
 /**
  * 用户密码，存储：
- *    加密后的密码
- *    盐值
+ * 加密后的密码
+ * 盐值
  */
 @Setter
 @Getter
@@ -17,40 +18,37 @@ public class UserEncryptPassword {
     /**
      * 加密后的密码
      */
-    private String encryptPassword;
-    
-    /**
-     * 盐值
-     */
-    private String salt;
+    private String encryptedPassword;
     
     
     /**
      * 保存盐值和加密后的密码，且不会存储明文密码
+     *
      * @param password 明文密码
-     * @param salt 盐值
+     * @param salt     盐值
      */
     public UserEncryptPassword(String password, String salt) {
-        this.salt = salt;
-        this.encryptPassword = encryptPassword(password);
+        this.encryptedPassword = encryptPassword(password, salt);
     }
     
     /**
      * 校验密码是否正确
+     *
      * @param password 明文密码
      * @return true-密码正确; false-密码错误
      */
-    public boolean isCorrect(String password) {
-        return encryptPassword(password).equals(encryptPassword);
+    public boolean isCorrect(String password, String salt) {
+        return StringUtils.equals(encryptPassword(password, salt), encryptedPassword);
     }
     
     
     /**
      * 加密密码
+     *
      * @param password 明文密码
      * @return 加密后的密码
      */
-    private String encryptPassword(String password) {
+    private String encryptPassword(String password, String salt) {
         return DigestUtils.md5DigestAsHex((password + salt).getBytes());
     }
 }
--- account-domain/src/main/java/org/mrzhuyk/practice/domain/user/model/UserEntity.java
+++ account-domain/src/main/java/org/mrzhuyk/practice/domain/user/model/UserEntity.java
@@ -8,7 +8,7 @@ import org.mrzhuyk.practice.util.RandomUtil;
 @Getter
 @Setter
 public class UserEntity {
-
+    
     private UserInfo userInfo;
     
     /**
@@ -18,6 +18,7 @@ public class UserEntity {
     
     /**
      * 传入明文密码，并且生成随机盐值对其加密，不会存储明文密码
+     *
      * @param password 明文密码
      */
     public void setPassword(String password) {
@@ -29,16 +30,17 @@ public class UserEntity {
      * @return 加密后的密码
      */
     public String getPassword() {
-        return userEncryptPassword.getEncryptPassword();
+        return userEncryptPassword.getEncryptedPassword();
     }
     
     
     /**
      * 校验密码是否正确
+     *
      * @param password 要验证的明文密码
      * @return true-密码正确; false-密码错误
      */
     public boolean isCorrectPassword(String password) {
-        return userEncryptPassword.isCorrect(password);
+        return userEncryptPassword.isCorrect(password, userInfo.getSalt());
     }
 }
